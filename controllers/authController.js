0c55cb11897b133fc4c030a2624775c25710cc0a
Abhishek <abmaurya07@gmail.com>
2024-07-26 20:55:14 +0530

    refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const jwtSecret = process.env.JWT_SECRET;
+const jwtRefreshSecret = process.env.JWT_REFRESH_SECRET;
  
 
 exports.signup = async (req, res) => {
@@ -11,7 +12,8 @@ exports.signup = async (req, res) => {
     const user = new User({ username, password });
     await user.save();
     const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1h' });
-    res.status(201).json({ token });
+    const refreshToken = jwt.sign({ id: user._id }, jwtRefreshSecret, { expiresIn: '7d' });
+    res.status(201).json({ token, refreshToken });
   } catch (error) {
     console.log('Error:', error);
     res.status(500).json({ error: error.message });
@@ -26,8 +28,23 @@ exports.login = async (req, res) => {
       return res.status(400).json({ message: 'Invalid credentials' });
     }
     const token = jwt.sign({ id: user._id }, jwtSecret, { expiresIn: '1h' });
-    res.status(200).json({ token });
+    const refreshToken = jwt.sign({ id: user._id }, jwtRefreshSecret, { expiresIn: '7d' });
+    res.status(200).json({ token, refreshToken });
   } catch (error) {
     res.status(500).json({ error: error.message });
+  }
+};
+
+
+exports.refreshToken = async (req, res) => {
+  const { token } = req.body;
+  if (!token) return res.status(401).json({ message: 'Unauthorized' });
+
+  try {
+    const payload = jwt.verify(token, jwtRefreshSecret);
+    const newToken = jwt.sign({ id: payload.id }, jwtSecret, { expiresIn: '1h' });
+    res.status(200).json({ token: newToken });
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized' });
   }
 };
